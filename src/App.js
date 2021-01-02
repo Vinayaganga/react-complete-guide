aefe852cf6cabecb79ee2ab1f333173f5a1e993d
Vinayaganga P <vinayaganga@gmail.com>
2021-01-02 11:05:59 +0530

    feat: flexible lists

--- src/App.js
+++ src/App.js
@@ -18,13 +18,19 @@ class App extends Component {
     persons.splice(index,1);
     this.setState({persons:persons});
   }
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex= this.state.persons.findIndex(p=>{
+      return p.id===id
+    })
+    const person= {
+      ...this.state.persons[personIndex]
+    };
+    person.name= event.target.value;
+    const persons=[...this.state.persons]
+    persons[personIndex]=person;
+    // const person = Object.assign({}, this.state.persons[personIndex]);
     this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
+      persons: persons
     } )
   }
   togglePersonsHandler = () =>{
@@ -44,7 +50,7 @@ class App extends Component {
     if(this.state.showPersons){
       persons=( <div>
         {this.state.persons.map((person,index)=>{
-          return <Person click= {()=>this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id}/>
+          return <Person click= {()=>this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} changed={(event)=>this.nameChangedHandler(event, person.id)} />
         })}
         </div>);
     }
