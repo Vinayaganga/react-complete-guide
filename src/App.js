adb82f86d1d17e27b2ae7140958f56ea87fc4322
Vinayaganga P <vinayaganga@gmail.com>
2021-01-02 10:50:09 +0530

    feat: updating state immutably

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
     showPersons:false
   }
   deletePersonHandler = (index)=>{
-    const persons= this.state.persons;
+   // const persons= this.state.persons.slice();
+   const persons= [...this.state.persons];
     persons.splice(index,1);
     this.setState({persons:persons});
   }
