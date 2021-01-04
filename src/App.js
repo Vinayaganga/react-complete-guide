a7601b457fbbeaa9aef65cada110a73e29272b4e
Vinayaganga P <vinayaganga@gmail.com>
2021-01-04 18:17:55 +0530

    feat: media queries with radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
+import Radium,{ StyleRoot} from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -76,7 +76,8 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
+      <StyleRoot>
+        <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <button 
@@ -84,6 +85,7 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
