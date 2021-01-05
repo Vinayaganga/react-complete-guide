2c39e05b8efb69c8d2d83d4729c00c4c300ab344
Vinayaganga P <vinayaganga@gmail.com>
2021-01-05 09:23:20 +0530

    feat: more on styled component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 /* import Radium,{ StyleRoot} from 'radium'; */
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color:white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover  {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -40,7 +54,7 @@ class App extends Component {
   }
 
   render () {
-    const style = {
+    /* const style = {
       backgroundColor: 'green',
       color:"white",
       font: 'inherit',
@@ -52,7 +66,7 @@ class App extends Component {
         color: 'black'
       }
 
-    };
+    }; */
     let persons=null;
     if(this.state.showPersons){
       persons=( <div>
@@ -60,11 +74,11 @@ class App extends Component {
           return <Person click= {()=>this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} changed={(event)=>this.nameChangedHandler(event, person.id)} />
         })}
         </div>);
-        style.backgroundColor='red';
+        /* style.backgroundColor='red';
         style[':hover']={
           backgroundColor:'lightred',
           color:'black'
-        };
+        }; */
     }
 
     const classes = [];
@@ -80,9 +94,8 @@ class App extends Component {
         <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton 
+          onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
           {persons}
       </div>
       /* </StyleRoot> */
