aef00dcb3a1fe9b45223a2fef10a5cecd5b4662a
Vinayaganga P <vinayaganga@gmail.com>
2021-01-12 08:41:01 +0530

    feat: better project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 /* import Radium,{ StyleRoot} from 'radium'; */
 /* import styled from 'styled-components'; */
 import classes from  './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 // import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 /* const StyledButton = styled.button`
