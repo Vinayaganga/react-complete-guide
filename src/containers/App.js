bb00d0723038ce537e84b0e6e1b2a5c75820a8a6
Vinayaganga P <vinayaganga@gmail.com>
2021-01-12 09:27:03 +0530

    feat: class vs functional componets

--- src/containers/App.js
+++ src/containers/App.js
@@ -51,7 +51,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
-          <Cockpit showPersons= {this.state.showPersons} persons={this.state.persons} clicked= {this.togglePersonsHandler}/>
+          <Cockpit title= {this.props.appTitle} showPersons= {this.state.showPersons} persons={this.state.persons} clicked= {this.togglePersonsHandler}/>
           {persons}
       </div>
       
